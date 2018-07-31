636f333d04904d52ddcea84e95b8b003ecf2487c
Woohyun, Jo <darkrubyeye@gmail.com>
2018-07-31 19:28:03 +0900

    기본 코드

---
+++ src/main/java/com/rubycorp/es/project_es_demo/model/Weather.java
@@ -0,0 +1,13 @@
+package com.rubycorp.es.project_es_demo.model;
+
+import lombok.Data;
+
+/**
+ * Created by woohyunjo on 2018-07-31.
+ */
+@Data
+public class Weather {
+    private String city;
+    private double temperature;
+    private String season;
+}
