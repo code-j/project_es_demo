5197c5f4e1cb9daa7c3335f375b36bbaf2854aa9
Woohyun, Jo <darkrubyeye@gmail.com>
2018-07-31 19:18:14 +0900

    기본 코드

---
+++ src/main/java/com/rubycorp/es/project_es_demo/ProjectEsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.rubycorp.es.project_es_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEsDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/rubycorp/es/project_es_demo/controller/ElasticSearchTestController.java
@@ -0,0 +1,10 @@
+package com.rubycorp.es.project_es_demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by woohyunjo on 2018-07-31.
+ */
+@RestController
+public class ElasticSearchTestController {
+}
